735c6f5c27b0c357eb090788e2ab808288494f6d
wendongchao <dongchao1860369@sina.com>
2023-08-07 15:14:01 +0800

    注释

--- itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/store/ICommodity.java
+++ itstack-demo-design-1-02/src/main/java/org/itstack/demo/design/store/ICommodity.java
@@ -4,6 +4,15 @@ import java.util.Map;
 
 public interface ICommodity {
 
+    /**
+     *
+     * @author dongchao
+     * @date 2023/8/7 15:09
+     * @param uId 用户ID
+     * @param commodityId 奖品ID
+     * @param bizId 业务ID
+     * @param extMap 扩展字段
+     */
     void sendCommodity(String uId, String commodityId, String bizId, Map<String, String> extMap) throws Exception;
 
 }
