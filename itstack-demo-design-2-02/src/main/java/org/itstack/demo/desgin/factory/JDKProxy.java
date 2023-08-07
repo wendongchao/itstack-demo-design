cb712689c57509e95df787490cc48d627ea61302
wendongchao <dongchao1860369@sina.com>
2023-08-07 16:19:35 +0800

    注释

--- itstack-demo-design-2-02/src/main/java/org/itstack/demo/desgin/factory/JDKProxy.java
+++ itstack-demo-design-2-02/src/main/java/org/itstack/demo/desgin/factory/JDKProxy.java
@@ -8,8 +8,15 @@ public class JDKProxy {
     public static <T> T getProxy(Class<T> interfaceClass, ICacheAdapter cacheAdapter) throws Exception {
         InvocationHandler handler = new JDKInvocationHandler(cacheAdapter);
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Class<?>[] classes = interfaceClass.getInterfaces();
-        return (T) Proxy.newProxyInstance(classLoader, new Class[]{classes[0]}, handler);
+//        Class<?>[] classes = interfaceClass.getInterfaces();
+//        return (T) Proxy.newProxyInstance(classLoader, new Class[]{classes[0]}, handler);
+        /*
+        传入三个参数
+            类加载器
+            需要代理的接口class
+            实现了InvocationHandler的代理对象
+         */
+        return (T) Proxy.newProxyInstance(classLoader, new Class[]{interfaceClass}, handler);
     }
 
 }
--- itstack-demo-design-2-02/src/test/java/org/itstack/demo/desgin/test/ApiTest.java
+++ itstack-demo-design-2-02/src/test/java/org/itstack/demo/desgin/test/ApiTest.java
@@ -12,12 +12,12 @@ public class ApiTest {
     @Test
     public void test_CacheService() throws Exception {
 
-        CacheService proxy_EGM = JDKProxy.getProxy(CacheServiceImpl.class, new EGMCacheAdapter());
+        CacheService proxy_EGM = JDKProxy.getProxy(CacheService.class, new EGMCacheAdapter());
         proxy_EGM.set("user_name_01", "小傅哥");
         String val01 = proxy_EGM.get("user_name_01");
         System.out.println("测试结果：" + val01);
 
-        CacheService proxy_IIR = JDKProxy.getProxy(CacheServiceImpl.class, new IIRCacheAdapter());
+        CacheService proxy_IIR = JDKProxy.getProxy(CacheService.class, new IIRCacheAdapter());
         proxy_IIR.set("user_name_01", "小傅哥");
         String val02 = proxy_IIR.get("user_name_01");
         System.out.println("测试结果：" + val02);
